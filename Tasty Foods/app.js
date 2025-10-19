e05eb12e50d5c0ab9d6935c7cd482350c32bb90e
hammedfuhad332-alt <hammedfuhad332@gmail.com>
2025-10-19 09:53:22 -0700

    Add files via upload

---
+++ Tasty Foods/app.js
@@ -0,0 +1,29 @@
+// add to cart 
+let cartQuantity = 0;
+
+function addCart(){
+    cartQuantity++;
+
+    document.getElementById('cqty').
+    innerHTML = cartQuantity;
+}
+// function addcart(produt){
+//     if(produt === 'burger'){
+//         cartQuantity++
+
+//         document.getElementById('caty').
+//         innerHTML = caetQuantity;
+//     }else if( product === 'pizza'){
+//         cartQuantity++;
+//     }
+//         document.getElementById('caty').
+//         innerHTML = caetQuantity;
+//     } else if( product === 'cake'){
+//         cartQuantity++;
+//     }
+//         document.getElementById('caty').
+//         innerHTML = caetQuantity;
+//     } else if( product === 'burger'){
+//         cartQuantity++;
+//     }
+// }
